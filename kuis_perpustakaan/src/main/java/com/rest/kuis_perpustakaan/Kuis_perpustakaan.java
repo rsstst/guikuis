0e7a3521a8b76f9f29984617d26564cbb75bb656
rsstst <snepshot551@gmail.com>
2023-12-24 11:10:00 +0700

    initial commit

---
+++ kuis_perpustakaan/src/main/java/com/rest/kuis_perpustakaan/Kuis_perpustakaan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rest.kuis_perpustakaan;
+
+/**
+ *
+ * @author rsstst
+ */
+public class Kuis_perpustakaan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
